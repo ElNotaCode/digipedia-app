49a7e5d5654837875d50da4dad69c1d95a87f7a2
Eloi Martorell Martín <eloi.martin174@gmail.com>
2022-07-05 13:00:38 +0200

    Angular project creado y capacitor instalado

---
+++ android/app/src/main/java/com/digimonpedia/MainActivity.java
@@ -0,0 +1,5 @@
+package com.digimonpedia;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.digimonpedia',
+  appName: 'Digipedia',
+  webDir: 'dist/digipedia-app',
+  bundledWebRuntime: false,
+};
+
+export default config;
