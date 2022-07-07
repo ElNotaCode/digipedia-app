aeb4dc4d68054c2046e4b12d84d96a9a254b10fd
Eloi Martorell Martín <eloi.martin174@gmail.com>
2022-07-07 13:58:30 +0200

    Nav arreglada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DigimonListComponent } from './components/digimon-list/digimon-list.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  //wildcard route, si ponen un path inventado les redirigirá aquí
-  //TODO: 404
-  { path: '**', component: DigimonListComponent },
   //TODO: Home
-  { path: '', component: DigimonListComponent },
+  { path: '', component: HomeComponent },
+  //Lista de todos los digimon
+  { path: 'digimon-list', component: DigimonListComponent },
+  //wildcard route, si ponen un path inventado les redirigirá aquí
+  //¡IMPORTANTE! poner al final
+  //TODO: 404
+  { path: '**', component: HomeComponent },
 ];
 
 @NgModule({
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,60 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  search: string = '';
+
+  levels: string[] = [
+    'Fresh',
+    'In Training',
+    'Rookie',
+    'Champion',
+    'Ultimate',
+    'Mega',
+    'Armor',
+  ];
+
+  buscarDigimon() {
+    let searchStr = this.tratarDatos();
+
+    let esLevel = this.comprobarNivel(searchStr);
+
+    //Ir a la ruta correspondiente
+    if (esLevel) {
+    } else {
+    }
   }
 
+  tratarDatos(): string {
+    //Tratar datos ya que la busqueda es case sensitive
+    //Primera mayus las demás minus
+    let searchStr =
+      this.search.charAt(0).toUpperCase() + this.search.slice(1).toLowerCase();
+
+    //En caso de que sea In Training
+    if (searchStr.charAt(2) == ' ') {
+      searchStr.charAt(3).toUpperCase;
+    }
+
+    return searchStr;
+  }
+
+  comprobarNivel(searchStr: string): boolean {
+    //comprobar si es un nivel
+    //Fresh, In Training, Rookie, Champion, Ultimate, Mega, Armor
+
+    for (let i = 0; i < this.levels.length; i++) {
+      if (this.levels[i] == searchStr) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
